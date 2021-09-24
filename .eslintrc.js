a3f559486c05740718a86fe58fb2b1c82c9662c0
LAPTOP-OLIVER <jamols09@gmail.com>
2021-09-24 17:58:40 +0800

    ignore files

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/script-setup-uses-vars': 'error',
+  },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly',
+    $ref: 'writable',
+    $computed: 'writable',
+    $raw: 'readonly',
+    withDefaults: 'readonly',
+    $fromRefs: 'readonly',
+  },
+  overrides: [
+    {
+      files: ['*.md'],
+      parser: 'markdown-eslint-parser',
+      extends: ['plugin:md/recommended', 'prettier'],
+    },
+  ],
+}
