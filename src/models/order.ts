ff58bfda99759c20f34b4dab0be4a07b8fb7ec5a
LAPTOP-OLIVER <jamols09@gmail.com>
2021-09-26 10:27:08 +0800

    added orde type declaration

---
+++ src/models/order.ts
@@ -0,0 +1,25 @@
+// Creat Order Tabs
+export type OrderTabs = 'customer' | 'item' | 'computation'
+
+// Customer info
+export interface CustomerInfo {
+  firstName: string
+  middleName?: string
+  lastName?: string
+  country: string
+  stateRegion: string
+  province: string
+  city: string
+  line1: string
+  line2?: string
+  mobile?: string
+  telephone?: string
+  postal?: number | null
+}
+
+// Item Attr
+export interface OrderItems {
+  code: string
+  quantity: number
+}
+export type OrderItemsArray = OrderItems[]
