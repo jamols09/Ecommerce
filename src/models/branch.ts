aeff5593cf66f79563a4bf334f6642a1a9b5d68e
LAPTOP-OLIVER <jamols09@gmail.com>
2021-09-26 22:13:53 +0800

    added branch types declaration

---
+++ src/models/branch.ts
@@ -0,0 +1,21 @@
+/****************************
+ * BRANCH TYPES DECLARATION *
+ ****************************/
+
+export interface GeoLocation {
+  longitude: number | null
+  latitude: number | null
+}
+
+export interface Branch {
+  name: string
+  code: string
+  country: string
+  regionState: string
+  city: string
+  addressLine1: string
+  addressLine2?: string | null
+  postal: number | null
+  telephone: string | null
+  geolocation: GeoLocation
+}
