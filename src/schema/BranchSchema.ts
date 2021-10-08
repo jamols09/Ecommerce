ecbb3badfa3b9dc9d775e77497eb70a5ffb85c47
LAPTOP-OLIVER <jamols09@gmail.com>
2021-10-08 16:28:59 +0800

    refactor: change branch schema requirements

--- src/schema/BranchSchema.ts
+++ src/schema/BranchSchema.ts
@@ -1,20 +1,20 @@
 import * as yup from 'yup'
 
 const BranchSchema = yup.object().shape({
-  name: yup.string().min(1).nullable().required('Branch name is required'),
-  code: yup.string().min(1).nullable().notRequired(),
+  name: yup.string().nullable().required('Branch name is required'),
+  code: yup.string().min(4).nullable().notRequired(),
   telephone: yup.string().nullable().notRequired(),
   mobile: yup.string().nullable().notRequired(),
-  country: yup.string().min(1).nullable().required('Coutry is required'),
+  country: yup.string().nullable().required('Coutry is required'),
   regionState: yup
     .string()
-    .min(1)
+
     .nullable()
     .required('Region or state is required'),
-  city: yup.string().min(1).nullable().required('City is required'),
+  city: yup.string().nullable().required('City is required'),
   addressLine1: yup
     .string()
-    .min(1)
+
     .nullable()
     .required('Address line 1 is required'),
   addressLine2: yup.string().nullable().notRequired(),
