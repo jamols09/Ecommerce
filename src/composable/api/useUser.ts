fc32400144b11b74477015f5127b292bd0a61f78
LAPTOP-OLIVER <jamols09@gmail.com>
2021-10-27 23:53:20 +0800

    refactor: implemented composable for error

--- src/composable/api/useUser.ts
+++ src/composable/api/useUser.ts
@@ -1,20 +1,22 @@
 import { ref } from 'vue'
+import useErrorNotification from './useErrorNotification'
 import { useApi } from '/@src/composable/useApi'
 
-const createResponse = ref([])
+const createResponse = ref()
 
 export function useUser() {
   const api = useApi()
+
   /**
    * @param data object
    * @returns HTTP status or error message
    */
   const create = async (user: any): Promise<any> => {
     try {
-      const { data } = await api.post('/v1/employees', user)
+      const { data } = await api.post('/v1/users', user)
       createResponse.value = data
     } catch (err: any) {
-      createResponse.value = err
+      useErrorNotification.error(err.response.data)
     }
   }
 
