05f41a757b8c683e86756925a6f5cae633682ebb
LAPTOP-OLIVER <jamols09@gmail.com>
2021-10-29 08:27:30 +0800

    feat: added error notification category composable

--- src/composable/api/useCategory.ts
+++ src/composable/api/useCategory.ts
@@ -1,36 +1,37 @@
 import { ref } from 'vue'
 import { useApi } from '/@src/composable/useApi'
+import useErrorNotification from './useErrorNotification'
+import useNotyf from '../useNotyf'
 
-const fetchResponse = ref([])
-const createResponse = ref([])
+const fetchResponse = ref()
+const createResponse = ref()
+const notif = useNotyf()
 
 export function useCategory() {
   const api = useApi()
 
   /**
-   * @param data object
+   * @param object category
    * @returns HTTP status or error message
    */
-  const create = async (category: any): Promise<any> => {
+  const create = async (category: any) => {
     try {
       const { data } = await api.post('/v1/category', category)
       createResponse.value = data
+      notif.success(`Category <b>${data.body.name}</b> added.`)
     } catch (err: any) {
-      createResponse.value = err
+      useErrorNotification.error(err.response.data)
     }
   }
 
-  const fetch = async (): Promise<any> => {
+  const fetch = async () => {
     try {
       const { data } = await api.get('/v1/category')
       fetchResponse.value = data
     } catch (err: any) {
-      fetchResponse.value = err
+      useErrorNotification.error(err.response.data)
     }
   }
-
-  // notyf.success(`Employee <b><u> ${inputs.firstname} </u></b> added.`)
-  // notyf.error(`Something wong here.`)
 
   return {
     fetchResponse,
