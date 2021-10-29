855738b96e61b13f8847ff04994d43b67957df54
LAPTOP-OLIVER <jamols09@gmail.com>
2021-10-29 08:28:49 +0800

    refactor: commented for loop temporarily, reapplied foreach due to error being skipped

--- src/composable/api/useErrorNotification.ts
+++ src/composable/api/useErrorNotification.ts
@@ -3,16 +3,31 @@ const notif = useNotyf()
 class useErrorNotification {
   error({ errors }: any) {
     console.log(errors)
-    const vals = Object.keys(errors).map((key) => errors[key])
-    vals.forEach((e) => {
-      if (e.length > 1) {
-        e.forEach((element: any) => {
-          notif.error(`${element}`)
-        })
-        return
-      }
-      notif.error(`${e}`)
-    })
+    try {
+      const result = Object.keys(errors).map((key) => errors[key])
+      result.forEach((e) => {
+        if (e.length > 1) {
+          e.forEach((element: any) => {
+            notif.error(`${element}`)
+          })
+          return
+        }
+        notif.error(`${e}`)
+      })
+    } catch (err) {
+      console.error(err)
+      notif.error(`${err}`)
+    }
+
+    // for (let a = 0; a < result.length; a++) {
+    //   if (result[a].length > 1) {
+    //     for (let b = 0; b < result[a].length; b++) {
+    //       notif.error(`${result[b]}`)
+    //     }
+    //     return
+    //   }
+    //   notif.error(`${a}`)
+    // }
   }
 }
 
