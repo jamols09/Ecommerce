3e6dd2399128e7ad86aece7edfae53a0625a6d11
LAPTOP-OLIVER <jamols09@gmail.com>
2021-11-07 18:39:37 +0800

    feat: add api call for department and item/product

---
+++ src/composable/api/useDepartment.ts
@@ -0,0 +1,29 @@
+import { ref } from 'vue'
+import { useApi } from '/@src/composable/useApi'
+import useErrorNotification from './useErrorNotification'
+import useNotyf from '../useNotyf'
+
+const createResponse = ref()
+
+export function useItem() {
+  const api = useApi()
+
+  /**
+   * @param object item
+   * @returns HTTP status or error message
+   */
+  const create = async (e: any): Promise<any> => {
+    try {
+      const { data } = await api.post('/v1/department', e)
+      createResponse.value = data
+      useNotyf().success(`Department <b>${data.body.name}</b> added.`)
+    } catch (err: any) {
+      useErrorNotification.error(err.response.data)
+    }
+  }
+
+  return {
+    createResponse,
+    create,
+  }
+}
