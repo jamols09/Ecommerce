3b638167432fdcec9f9f174d8066e4ead0e73c8c
LAPTOP-OLIVER <jamols09@gmail.com>
2021-11-08 16:16:31 +0800

    feat: add loading state for useItem

--- src/composable/api/useItem.ts
+++ src/composable/api/useItem.ts
@@ -4,6 +4,7 @@ import useErrorNotification from './useErrorNotification'
 import useNotyf from '../useNotyf'
 
 const createResponse = ref()
+const isLoading = ref(false)
 
 export function useItem() {
   const api = useApi()
@@ -13,17 +14,20 @@ export function useItem() {
    * @returns HTTP status or error message
    */
   const create = async (e: any): Promise<any> => {
+    isLoading.value = true
     try {
       const { data } = await api.post('/v1/item', e)
       createResponse.value = data
-      useNotyf().success(`Product <b>${data.body.name}</b> added.`)
+      useNotyf().success(`Product <b>${data.body}</b> added.`)
     } catch (err: any) {
       useErrorNotification.error(err.response.data)
     }
+    isLoading.value = false
   }
 
   return {
     createResponse,
+    isLoading,
     create,
   }
 }
