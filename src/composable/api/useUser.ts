629cfa3fa1b73ac44a4bb28ce98c6cad522e8686
LAPTOP-OLIVER <jamols09@gmail.com>
2021-11-10 22:48:21 +0800

    refactor: change url query format to query string

--- src/composable/api/useUser.ts
+++ src/composable/api/useUser.ts
@@ -32,9 +32,7 @@ export function useUser() {
   const table = async (e?: any): Promise<any> => {
     isLoading.value = true
     try {
-      const { data } = await api.get(
-        `/v1/users?page=${e.page}&row=${e.row}&type=${e.type}&q=${e.query}&col=${e.column}&order=${e.order}&u=${e.account}`
-      )
+      const { data } = await api.get(`/v1/users`, { params: e })
       tableResponse.value = data
     } catch (err: any) {
       useErrorNotification.error(err.response.data)
