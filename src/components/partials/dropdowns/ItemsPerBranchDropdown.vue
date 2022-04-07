2cea98b1fa4462058988595ff6e20b5e8acbdc0c
LAPTOP-OLIVER <jamols09@gmail.com>
2022-04-07 11:15:32 +0800

    refactor: remove unncessary code

--- src/components/partials/dropdowns/ItemsPerBranchDropdown.vue
+++ src/components/partials/dropdowns/ItemsPerBranchDropdown.vue
@@ -1,19 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-// interface IDetails {
-//   item_id: number
-//   branch_id: number
-//   id: number
-//   is_active: number
-//   is_display_qty: number
-//   quantity: number
-//   quantity_warn: number
-//   price: number
-// }
-
 interface IActionDropdownProps {
-  // details: IDetails
   titleDeactivate?: string
   messageDeactivate?: string
   titleEdit?: string
@@ -31,16 +19,6 @@ interface IActionDropdownProps {
 let obj = {}
 
 const props = withDefaults(defineProps<IActionDropdownProps>(), {
-  // details: () => ({
-  //   item_id: 0,
-  //   branch_id: 0,
-  //   id: 0,
-  //   is_active: 0,
-  //   is_display_qty: 0,
-  //   quantity: 0,
-  //   quantity_warn: 0,
-  //   price: 0,
-  // }),
   titleDeactivate: 'Deactivate',
   messageDeactivate: 'Change item status',
   titleEdit: 'Edit',
@@ -71,25 +49,6 @@ const onActivate = async (e: number) => {
 <template>
   <VDropdown icon="feather:more-vertical" right spaced>
     <template #content>
-      <!-- <RouterLink
-        :to="{
-          path: props.editRoute + props.actionId,
-        }"
-        role="menuitem"
-        class="dropdown-item is-media"
-      >
-        <div class="icon">
-          <i aria-hidden="true" class="lnil lnil-cog"></i>
-        </div>
-        <div class="meta">
-          <span>{{ props.titleEdit }}</span>
-          <span>
-            <small>{{ props.messageEdit }}</small>
-          </span>
-        </div>
-      </RouterLink> -->
-
-      <!-- <hr class="dropdown-divider" /> -->
       <template v-if="props.isActive === 1">
         <a
           href="#"
